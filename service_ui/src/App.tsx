e9637b4772663fc4516e205ed69e4c943aae475c
jouwana <41836591+jouwana@users.noreply.github.com>
2022-12-12 22:36:10 +0200

    about page changes

--- service_ui/src/App.tsx
+++ service_ui/src/App.tsx
@@ -72,7 +72,7 @@ function App() {
     return (
       <>
         <div className="App">
-            <button className='FAQ' onClick={()=>{setPageNumber(2)}}>FAQs</button>
+            <button className='FAQ' onClick={()=>{setPageNumber(2)}}>About</button>
             <img src={logo} className="App-logo" alt="logo" />
             <div className='search-button'>
               <TextField className='search-bar'
@@ -126,7 +126,25 @@ function App() {
     return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
-       FAQ
+      <div className='about-text'>
+        <p>
+          This project addresses the needs of Sikkuy-Aufoq (SA) 
+          - a shared Jewish and Arab nonprofit organization that 
+          works to advance equality and partnership between the 
+          Arab-Palestinian citizens of Israel - who, in their own words: 
+          work to educate and motivate the public at large and to shape 
+          public discourse through the media and digital spaces.
+        </p>
+        <p>
+          The MVP provides a dashboard that presents an easy way to query data, with advanced search options,
+          the results found can be sortable, queryable data/statistics.
+        </p>
+        <p>
+          This is done by scrapping targeted websites and
+          digital-newspapers for relevant articles and evaluating the intonation/meaning
+          of their content.
+        </p>
+       </div>
       <button className='go-back-button' onClick={()=>{setPageNumber(0)}}>go back</button>
     </div>)
   }
