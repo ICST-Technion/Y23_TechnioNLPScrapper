56a904914521a9dcbb092b7ac5f201db73e1d67e
jouwana <41836591+jouwana@users.noreply.github.com>
2022-12-26 16:23:33 +0200

    split about to its own page

--- service_ui/src/App.tsx
+++ service_ui/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './Images/logo.jpg';
 import './App.css';
 import { SearchPage } from './Components/QueryPage/SearchPage';
 import { BaseResults } from './Components/Results/BaseResults';
+import { About } from './About';
 
 export class advancedQueryData{
   includedWebsites:Map<number, string>;
@@ -74,29 +75,10 @@ function App() {
   }
   else if(pageNumber === 2){
     return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <div className='about-text'>
-        <p>
-          This project addresses the needs of Sikkuy-Aufoq (SA) 
-          - a shared Jewish and Arab nonprofit organization that 
-          works to advance equality and partnership between the 
-          Arab-Palestinian citizens of Israel - who, in their own words: 
-          work to educate and motivate the public at large and to shape 
-          public discourse through the media and digital spaces.
-        </p>
-        <p>
-          The MVP provides a dashboard that presents an easy way to query data, with advanced search options,
-          the results found can be sortable, queryable data/statistics.
-        </p>
-        <p>
-          This is done by scrapping targeted websites and
-          digital-newspapers for relevant articles and evaluating the intonation/meaning
-          of their content.
-        </p>
-       </div>
-      <button className='go-back-button' onClick={()=>{setPageNumber(0)}}>go back</button>
-    </div>)
+      <>
+        <About setPageNumber={setPageNumber} />
+      </>
+    )
   }
   else {
     return (<div> INCORRECT PAGE NUMBER </div>)
