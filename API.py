e4df6da69cf5f5c0d5e78618d23b536a01f18e39
Dalia Chulsky <62931065+arch-dimension@users.noreply.github.com>
2023-01-06 19:29:46 +0200

    tested API requests on thunderclient

--- API.py
+++ API.py
@@ -1,7 +1,6 @@
 # Using flask to make an api
 # import necessary libraries and functions
 from flask import Flask, jsonify, request, make_response
-import requests
 from SqlQueries import *
 from datetime import datetime
 # from google import search
@@ -14,27 +13,28 @@ app = Flask(__name__)
 def default_select_table():
     select_all = SQLQuery()
     table = select_all.select_articles_from_sql()
-    return jsonify(results=table)
+    return make_response(jsonify(results=table), 200)
 
 
 @app.route('/clear', methods=['POST'])
 def clear_table():
     clear_query = SQLQuery()
     clear_query.clear_table()
+    return make_response("OK", 200)
 
 
 # request of regular query
 @app.route('/query', methods=['POST'])
 def get_database_query():
-    query = request.json.get('Query', [])
+    query = request.json.get('Query', "")
+    #TODO:add the google search in here
 
-    return make_response("OK", 200)
+    return make_response(query, 200)
 
 
 def parse_json_and_strip(json_field):
     field_content = request.json.get(json_field, "")
-    [word.strip() for word in field_content.split(',')]
-    return field_content
+    return  [word.strip() for word in field_content.split(',')]
 
 
 def parse_table_rows(rows):
@@ -45,6 +45,11 @@ def parse_table_rows(rows):
 def advanced_search():
     # TODO: check format
     included_keywords = parse_json_and_strip('included_keywords')
+    #requires at least one keyword
+    if not included_keywords:
+        #return bad request error
+        return make_response("Searching requires at least one keyword", 400)
+
     excluded_keywords = parse_json_and_strip('excluded_keywords')
     query_executor = SQLQuery()
     table = query_executor.select_articles_from_sql(columns="keyword")
@@ -60,13 +65,15 @@ def advanced_search():
     websites_to_search = list((set(database_websites) | set(specified_websites)) - set(excluded_websites))
 
     # assumption: time range would be just two dates separated by comma
-    time_range = parse_json_and_strip('time_range')
-    datetime_range = [datetime.strptime(date_string, "%Y-%m-%d") for date_string in time_range]
+    time_range = parse_json_and_strip('date_range')
+    #no dates were passed
+    if time_range==[""]:
+        datetime_range=[datetime(year=datetime.now().year-1,month= 1, day=1),datetime.now()]
+    else:
+        datetime_range = [datetime.strptime(date_string, "%Y-%m-%d") for date_string in time_range]
 
     links_to_scrap=[]
-    # for website in websites_to_search:
-    #     query = website
-    #     links_to_scrap.extend(search(query, tld="com", lang=['en', 'he'], num=10, start=1))
+    #TODO: search for queries and stuff here
 
     # not adding specified statistics yet, because there is only counter for now
     return make_response("OK", 200)
@@ -74,4 +81,5 @@ def advanced_search():
 
 # driver function
 if __name__ == '__main__':
+
     app.run(debug=True)
