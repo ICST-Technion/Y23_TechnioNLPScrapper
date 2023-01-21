c963067fdc48cf830d5a831ba553287d891da1d0
Dalia Chulsky <62931065+arch-dimension@users.noreply.github.com>
2023-01-21 12:33:29 +0200

    installed axios and a clear function

---
+++ server/src/consts.ts
@@ -0,0 +1,4 @@
+
+export const query_request='/query'
+export const advanced_search_request='/advancedSearch'
+export const clear_request='/clear'
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import cors from 'cors';
 import knex from 'knex';
 import dotenv from 'dotenv';
 import {client} from "./elephantsql.js"
+import axios, {isCancel, AxiosError} from 'axios';
+import {clear_request} from "./consts.js"
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -12,6 +14,33 @@ const app:Express = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+
+
+async function clear_table() {
+  const body={}
+  try {
+    const response = await axios.request({
+      method:'post',
+     url:clear_request,
+      body
+    }
+      
+      );
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+app.get('/query', async (req: Request, res: Response) => {
+  try {
+    const response = 
+    console.log(response);
+    clear_table()
+} catch (err) {
+    console.log(err);
+}
+});
 
 app.get('/rows', async (req: Request, res: Response) => {
   try {
