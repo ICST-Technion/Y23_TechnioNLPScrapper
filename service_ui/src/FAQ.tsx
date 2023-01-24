dd17450b12d02c85cac30dd187244301df371452
layan10 <layanhaddad5@gmail.com>
2023-01-24 02:46:13 +0200

    fix weird big with FAQs

--- service_ui/src/FAQ.tsx
+++ service_ui/src/FAQ.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { AQObject } from './FAQsPage';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandMore from '@mui/icons-material';
 
 export interface AQComponentProps {
    faq:AQObject;
@@ -18,7 +18,7 @@ export const AQComponent: React.FC<AQComponentProps> = ({faq, index, toggleFAQ})
 		>
 			<div className="faq-question" style={{display: 'flex', justifyContent:'space-between', alignItems: 'center'}}>
 				{faq.question}
-				<ExpandMoreIcon />
+				
 			</div>
 			<div className="faq-answer">
 				{faq.answer}
