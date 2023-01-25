c65544313d0880280bc2818f8b360c2cd4ef7431
jouwana <41836591+jouwana@users.noreply.github.com>
2023-01-25 02:31:39 +0200

    set api address to render

--- server/src/consts.ts
+++ server/src/consts.ts
@@ -2,4 +2,4 @@
 export const query_request='/query'
 export const advanced_search_request='/advancedSearch'
 export const clear_request='/clear'
-export const api_address='http://localhost:10000'
+export const api_address='https://technionlpapi.onrender.com'
