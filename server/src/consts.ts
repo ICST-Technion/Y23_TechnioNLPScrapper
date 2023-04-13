9bff0d2a2cb65cbf43e32fe747a60bc78be343fa
Dalia Chulsky <62931065+arch-dimension@users.noreply.github.com>
2023-04-13 13:43:15 +0300

    changed the address to the render server

--- server/src/consts.ts
+++ server/src/consts.ts
@@ -2,4 +2,4 @@
 export const query_request='/query'
 export const advanced_search_request='/advancedSearch'
 export const clear_request='/clear'
-export const api_address='http://127.0.0.1:10000'
+export const api_address='https://technionlpapi.onrender.com'
