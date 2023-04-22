8ff935cba55df457e6a014e1ac2ea97f5bbacfe4
jouwana <41836591+jouwana@users.noreply.github.com>
2023-04-23 00:14:00 +0300

    remove test server

--- service_ui/src/Helpers/consts.ts
+++ service_ui/src/Helpers/consts.ts
@@ -3,5 +3,5 @@ export const RESULTS_PAGE = 1;
 export const FAQ_PAGE = 2;
 export const ADVANCED_SEARCH_PAGE = 3;
 
-//export const FE_SERVER = "https://technionlp-fe-service.onrender.com";
-export const FE_SERVER = "http://localhost:5000"; // for testing on local machine
+export const FE_SERVER = "https://technionlp-fe-service.onrender.com";
+//export const FE_SERVER = "http://localhost:5000"; // for testing on local machine
