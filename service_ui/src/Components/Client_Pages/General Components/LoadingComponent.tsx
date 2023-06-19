c420faf53a4cbed885c69436b7ca30b243312903
jouwana <41836591+jouwana@users.noreply.github.com>
2023-06-19 10:03:22 +0300

    add percentage indicator to loading bar #189

--- service_ui/src/Components/Client_Pages/General Components/LoadingComponent.tsx
+++ service_ui/src/Components/Client_Pages/General Components/LoadingComponent.tsx
@@ -3,6 +3,7 @@ import { useNProgress } from '@tanem/react-nprogress'
 import LinearProgress from '@mui/material/LinearProgress'
 import Container from '@mui/material/Container'
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export interface LoadingComponentProps {
     isAnimating: boolean;
@@ -21,7 +22,12 @@ export interface LoadingComponentProps {
     return (
         <>
         <Container className="loading-container" hidden={isFinished} disableGutters={true} style={{transition: `opacity ${animationDuration}ms linear`}}>
-        <LinearProgress className="loading-bar" variant="determinate" value={progress * 100} style={{transitionDuration: `${animationDuration}ms`}} />
+          <Container className="loading-bar-container">
+          <LinearProgress className="loading-bar" variant="determinate" value={progress * 100} style={{transitionDuration: `${animationDuration}ms`}} />
+            <Typography variant="body2" color="text.secondary">{`${Math.round(
+              progress * 100,
+            )}%`}</Typography>
+            </Container>
         <Typography variant="h6" className="loading-message">{message}</Typography>
         </Container>
         </>
