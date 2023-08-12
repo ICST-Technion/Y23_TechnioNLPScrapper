fb7ac55ec61f073697614c8e301ed2f0511e2dfd
jouwana <41836591+jouwana@users.noreply.github.com>
2023-08-12 17:49:05 +0300

    fix cannot concat int to str error

--- API/API.py
+++ API/API.py
@@ -234,7 +234,7 @@ def do_search_query(category='1'):
     - category (str): The number of the current category we are scraping for.
     '''
     query = request.json.get('Query'+category, "")  # assuming Query is the keywords
-    table_id = request.json.get('id', "")
+    table_id = str(request.json.get('id', ""))
 
     # Parse the query
     query_dict = parse_google_search_query(query)
@@ -257,7 +257,7 @@ def do_search_query(category='1'):
             negative_keywords=negative_keywords
         )
     # Perform the search and scrape the links for each website
-    SQLQuery().generate_table(table_id)
+    SQLQuery().generate_table(str(table_id))
     for website in site_list:
         search_results = search_google(query, website)
         scrap_links(search_results, keyword_to_intonation,phrase_to_intonation,table_id,category)
@@ -397,7 +397,7 @@ def advanced_search_query(category='1'):
         return make_response("Searching requires at least one keyword", 400)
     #returns string
     keywords_to_search=request.json.get('included_keywords' + category, [])
-    table_id = request.json.get('id', '')
+    table_id = str(request.json.get('id', ''))
 
     websites_to_search = list(set(request.json.get('included_sites' + category,[])) | 
                               set(get_default_websites()))
@@ -442,7 +442,7 @@ def advanced_search_query(category='1'):
     exclude_query = ','.join(decoded_exclude)
     
 
-    SQLQuery().generate_table(table_id)
+    SQLQuery().generate_table(str(table_id))
     for website in websites_to_search:
         links_to_scrap = search_google(query, website, exclude_query)
         scrap_links(links_to_scrap,keyword_to_intonation,phrases_to_intonation,table_id,category)
