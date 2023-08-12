0366ae54d3388b0087b34cb773249b025b1da8d7
jouwana <41836591+jouwana@users.noreply.github.com>
2023-08-12 21:36:45 +0300

    return server addresses to cloud instead of localhost

--- service_ui/src/Helpers/consts.ts
+++ service_ui/src/Helpers/consts.ts
@@ -3,8 +3,8 @@ export const RESULTS_PAGE = 1;
 export const FAQ_PAGE = 2;
 export const ADVANCED_SEARCH_PAGE = 3;
 
-// export const FE_SERVER = "https://technio-nlp-server-pk6cdcur6a-zf.a.run.app";
-export const FE_SERVER = "http://localhost:5000"; // for testing on local machine
+export const FE_SERVER = "https://technio-nlp-server-pk6cdcur6a-zf.a.run.app";
+//export const FE_SERVER = "http://localhost:5000"; // for testing on local machine
 
 export const ENGLISH: number = 0;
 export const HEBREW: number = 1;
